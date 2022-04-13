37359bef839d9c240663318e93e99c0b3417587a
28188 <2818815189@qq.com>
2022-04-13 08:26:26 +0800

    保存

--- NettyChess/src/main/java/server/netty/NettyServer.java
+++ NettyChess/src/main/java/server/netty/NettyServer.java
@@ -2,6 +2,8 @@ package server.netty;
 
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -13,6 +15,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.util.Vector;
 
@@ -171,8 +174,22 @@ public class NettyServer extends JFrame implements ActionListener {
                     .childHandler(new ChannelInitializer<NioSocketChannel>() {
                         @Override
                         protected void initChannel(NioSocketChannel ch) throws Exception {
-                            System.out.println("--------");
-                            addName();
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelActive(ctx);
+                                    ctx.channel().read();
+                                    InetSocketAddress insocket = (InetSocketAddress) ctx.channel().remoteAddress();
+                                    String clientIp = insocket.getAddress().getHostAddress();
+                                    System.out.println("用户ip: " + clientIp + " 连接到了服务器");
+                                    addName();
+                                }
+
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    super.channelRead(ctx, msg);
+                                }
+                            });
                         }
                     }).bind(8888).sync().channel().closeFuture();
         } catch (InterruptedException e) {
--- Netty_Chess/core/src/kw/mulitplay/game/screen/LoadingScreen.java
+++ Netty_Chess/core/src/kw/mulitplay/game/screen/LoadingScreen.java
@@ -2,6 +2,7 @@ package kw.mulitplay.game.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
@@ -15,15 +16,20 @@ public class LoadingScreen extends BaseScreen {
     @Override
     public void show() {
         super.show();
-        Image image = new Image(new Texture("orange.png")){
-            @Override
-            public void act(float delta) {
-                super.act(delta);
-                System.out.println(getWidth());
-            }
-        };
-        image.setDebug(true);
+        Image image = new Image(new Texture("orange.png"));
         addActor(image);
+
+//        Texture texture = new Texture("orange.png");
+//        texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.ClampToEdge);
+//        TextureRegion region = new TextureRegion(texture);
+//
+//        region.setRegionWidth(1000);
+//
+//
+//        Image image = new Image(region);
+//        stage.addActor(image);
+
+
         stage.addAction(Actions.delay(1,Actions.run(
                 ()->{
                     Asset.ChessGame.setScreen(new GameScreen());
